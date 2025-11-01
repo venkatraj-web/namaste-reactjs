b1a82878c4b26333d518a2750dcb4808bdd04bfb
venkatraj-web <iamvenkatraj2k23@gmail.com>
2025-11-01 14:55:11 +0530

    core reactjs concept

---
+++ index.js
@@ -0,0 +1,39 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hi venkat from Reactjs"
+// ); // object
+
+// console.log(heading);
+
+/**
+ *
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1>hi h1 tag </h1>
+ *      <h2>hi h2 tag </h2>
+ *  </div>
+ *  <div id="child2">
+ *      <h1>hi h1 tag xhild2</h1>
+ *      <h2>hi h2 tag yhild2</h2>
+ *  </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "hi h1 tag"),
+    React.createElement("h2", {}, "hi h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "hi h1 tag xhild2"),
+    React.createElement("h2", {}, "hi h2 tag xhild2"),
+  ]),
+]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
