d2f46e48364aedcc857013058cabc97e4f5804b4
venkatraj-web <iamvenkatraj2k23@gmail.com>
2025-11-01 15:45:24 +0530

    parcel setup starting stage done!

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hi venkat from Reactjs"
-// ); // object
-
-// console.log(heading);
-
 /**
  *
  * <div id="parent">
- *  <div id="child">
- *      <h1>hi h1 tag </h1>
- *      <h2>hi h2 tag </h2>
- *  </div>
- *  <div id="child2">
- *      <h1>hi h1 tag xhild2</h1>
- *      <h2>hi h2 tag yhild2</h2>
- *  </div>
+ *      <div id="child">
+ *         <h1>I'm h1 tag</h1>
+ *         <h2>I'm h1 tag</h2>
+ *      </div>
+ * <div id="child2">
+ *         <h1>I'm h1 tag</h1>
+ *         <h2>I'm h1 tag</h2>
+ *      </div>
  * </div>
  *
+ * ReactElement(Object) => HTML(Browser Understands)
  */
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "hi h1 tag"),
-    React.createElement("h2", {}, "hi h2 tag"),
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "hi h1 tag xhild2"),
-    React.createElement("h2", {}, "hi h2 tag xhild2"),
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tags"),
   ]),
 ]);
 
-console.log(parent);
+//JSX
+
+console.log(parent); // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
